bed7030e83b2eac1f3826ce4dd584c3008bbdc11
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-09-15 11:07:21 +0600

    up for final

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,12 @@ use App\Http\Controllers\Admin\UserManageController;
 use App\Http\Controllers\Admin\UserReportController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\SendSms;
 use App\Http\Controllers\User\BookmarkController;
 use App\Http\Controllers\User\CommentController;
 use App\Http\Controllers\User\HeartController;
 use App\Http\Controllers\User\PostController;
 use App\Http\Controllers\User\ProfileController;
-use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Route;
-use NunoMaduro\Collision\Adapters\Phpunit\State;
 
 // public route for user
 Route::post('/register', [AuthController::class, 'register']);
@@ -27,7 +24,6 @@ Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/check-token', [AuthController::class, 'checkToken']);
-
 
 Route::middleware('auth:api')->group(function () {
     // private route for user
@@ -98,7 +94,6 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/user-search', [PostController::class, 'userSearch']);
         Route::get('/restaurant-search', [PostController::class, 'restaurantSearch']);
         Route::get('/view-restaurant/{id?}', [PostController::class, 'viewRestaurant']);
-
 
         // post
         Route::post('/create-post', [PostController::class, 'createPost']);
