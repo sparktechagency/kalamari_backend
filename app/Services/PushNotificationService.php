d5d904ba8c25138ede13e854780c4b5365293451
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-12-16 11:50:47 +0600

    fix

--- app/Services/PushNotificationService.php
+++ app/Services/PushNotificationService.php
@@ -11,32 +11,32 @@ class PushNotificationService
 
     public function __construct()
     {
-        $client = new Client();
-        $client->setAuthConfig(base_path(env('FIREBASE_CREDENTIALS')));
-        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        // $client = new Client();
+        // $client->setAuthConfig(base_path(env('FIREBASE_CREDENTIALS')));
+        // $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
-        $this->messaging = new FirebaseCloudMessaging($client);
+        // $this->messaging = new FirebaseCloudMessaging($client);
     }
     public function sendNotification($deviceToken, $title, $body, $data = [])
     {
-        $projectId = env('FIREBASE_PROJECT_ID');
+        // $projectId = env('FIREBASE_PROJECT_ID');
 
-        $data['status'] = 'active';
+        // $data['status'] = 'active';
 
-        $message = [
-            "message" => [
-                "token" => $deviceToken,
-                "notification" => [
-                    "title" => $title,
-                    "body"  => $body
-                ],
-                "data" => $data
-            ]
-        ];
+        // $message = [
+        //     "message" => [
+        //         "token" => $deviceToken,
+        //         "notification" => [
+        //             "title" => $title,
+        //             "body"  => $body
+        //         ],
+        //         "data" => $data
+        //     ]
+        // ];
 
-        return $this->messaging->projects_messages->send(
-            "projects/{$projectId}",
-            new FirebaseCloudMessaging\SendMessageRequest($message)
-        );
+        // return $this->messaging->projects_messages->send(
+        //     "projects/{$projectId}",
+        //     new FirebaseCloudMessaging\SendMessageRequest($message)
+        // );
     }
 }
